5bb9abb1e50ab8218335cacfad05dcf77ec9e067
lizhaowen <690591397@qq.com>
2025-02-06 20:46:03 +0800

    Initial Commit

---
+++ fill-light/fill_lightApp.swift
@@ -0,0 +1,32 @@
+//
+//  fill_lightApp.swift
+//  fill-light
+//
+//  Created by 李西西 on 2025/2/6.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct fill_lightApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ fill-lightTests/fill_lightTests.swift
@@ -0,0 +1,17 @@
+//
+//  fill_lightTests.swift
+//  fill-lightTests
+//
+//  Created by 李西西 on 2025/2/6.
+//
+
+import Testing
+@testable import fill_light
+
+struct fill_lightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
